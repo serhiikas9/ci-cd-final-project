841f2dd5a2ff0b02c016f92de5feaa0082c6d320
Serhii Kas <serhii.kashuba@gmail.com>
2024-02-25 16:54:46 -0500

    fix linting

--- service/routes.py
+++ service/routes.py
@@ -40,7 +40,7 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
+    counters = [dict(name=cc[0], counter=cc[1]) for cc in COUNTER.items()]
 
     return jsonify(counters)
 
@@ -54,7 +54,7 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
+        return abort(status.HTTP_409_CONFLICT, f"Counter {name} exists")
 
     COUNTER[name] = 0
 
@@ -75,7 +75,7 @@ def read_counters(name):
     app.logger.info("Request to Read counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} doesn't exist")
+        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} dsn't exist")
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
@@ -90,7 +90,7 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} doesn't exist")
+        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} dsn't exist")
 
     COUNTER[name] += 1
 
